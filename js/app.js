00acc8580719ffb36215ea22aebeb41aada5eb12
Andrii <Luckyhidalgo@gmail.com>
2021-03-09 16:36:04 +0200

    Revision.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(function(){
-	var DELTA = 1000;
+	var DELTA = 870;
 	
 	function helperOpacityCalc() {
 		var $scrollPercent = ($(document).scrollTop() / DELTA);
@@ -10,6 +10,49 @@ $(function(){
 	}
 	
 	helperOpacityCalc();
+	
+	const lightOrDark = (color) => {
+		// Variables for red, green, blue values
+		var r, g, b, hsp;
+		
+		// Check the format of the color, HEX or RGB?
+		if (color.match(/^rgb/)) {
+			// If RGB --> store the red, green, blue values in separate variables
+			color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
+			
+			r = color[1];
+			g = color[2];
+			b = color[3];
+		} else {
+			// If hex --> Convert it to RGB: http://gist.github.com/983661
+			color = +("0x" + color.slice(1).replace(color.length < 5 && /./g, "$&$&"));
+			
+			r = color >> 16;
+			g = (color >> 8) & 255;
+			b = color & 255;
+		}
+		
+		// HSP (Hue, Saturation, Perceived-brightness) equation from http://alienryderflex.com/hsp.html
+		hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
+		
+		// Using the HSP value, determine whether the color is light or dark
+		if (hsp > 127.5) {
+			return "light";
+		} else {
+			return "dark";
+		}
+	};
+	
+	let body = document.querySelector('body'),
+		styles = getComputedStyle(body);
+	
+	if(lightOrDark(styles['background-color']) === 'dark') {
+		$('.profile__info *').css({color: '#fff'});
+	}	else {
+		$('.profile__info *').css({color: '#000'});
+	}
+	
+	$('.profile__info').css({opacity: 1});
 	
 	$(window).on('resize scroll', function() {
 		helperOpacityCalc();
